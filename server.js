ac2bd1c194d006990f3ad4366d6e813f6944d1c9
Vickins1 <vickinstechnologies@gmail.com>
2024-12-25 19:40:14 +0300

    code

--- server.js
+++ server.js
@@ -24,7 +24,8 @@ const http = require('http');
 const server = http.createServer(app);
 app.set('trust proxy', 1);
 
-const uri = "mongodb://localhost:27017/LC-db";
+
+const uri = "mongodb://Admin:Kefini360@lease-captain-shard-00-00.ryokh.mongodb.net:27017,lease-captain-shard-00-01.ryokh.mongodb.net:27017,lease-captain-shard-00-02.ryokh.mongodb.net:27017/LC-db?ssl=true&replicaSet=atlas-67tjyi-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Lease-Captain";
 
 
 async function createDatabaseAndCollections() {
